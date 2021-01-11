f40e78f149a942155cce69ec08eba7d721e80e41
Neil Ricci <iccir919@gmail.com>
2021-01-11 08:57:49 -0800

    Pass all tests

--- bar-chart/script.js
+++ bar-chart/script.js
@@ -23,6 +23,13 @@ function drawBarChart(json) {
         .attr("width", w)
         .attr("height", h);
 
+    const div = d3
+        .select("body")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0)
+        .attr("id", "tooltip")
+
     const yScale = d3.scaleLinear()
         .domain([0, d3.max(dataset, (d) => d[1])])
         .range([h - padding, padding])
@@ -46,8 +53,9 @@ function drawBarChart(json) {
         .attr("id", "x-axis");
     
     svg.append("g")
-        .attr("transform", 
-        `translate(${padding}, 0)`)
+        .attr(
+            "transform", 
+            `translate(${padding}, 0)`)
         .call(yAxis)
         .attr("id", "y-axis");
 
@@ -58,4 +66,38 @@ function drawBarChart(json) {
         .attr("class", "bar")
         .attr("data-date", (d) => d[0])
         .attr("data-gdp", (d) => d[1])
+        .attr("x", (d, i) => ((w - (padding * 2)) / dataset.length) * i)
+        .attr("y", (d) => yScale(d[1]))
+        .attr("height", (d) => h - padding - yScale(d[1]))
+        .attr("width", () => ((w - (padding * 2)) / dataset.length) - .75)
+        .attr("transform", `translate(${padding},0)`)
+        .attr("fill", "DarkSeaGreen")
+        .on("mouseover", (event, d) => {
+            const date = new Date(d[0]);
+            div
+                .transition()
+                .duration(200)
+                .style("opacity", .9)
+            div
+                .html(`${date.getFullYear()} Q${quarterOfTheYear(date)} <br> ${formatAmount(d[1])}`)
+                .style("left", (event.pageX) + "px")
+                .style("top", (event.pageY - 28) + "px")
+                .attr("data-date", d[0])
+        })
+        .on("mouseout", () => {
+            div
+                .transition()
+                .duration(500)
+                .style("opacity", 0)
+        })
+
+}
+
+function quarterOfTheYear(date) {
+    const month = date.getMonth() + 1;
+    return (Math.ceil(month / 3));
+}
+
+function formatAmount(amount) {
+    return `$${amount.toLocaleString()} Billion`
 }
