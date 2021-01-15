43a2d8ae09d519bff29d1817f34b4b8a676c0f27
Neil Ricci <iccir919@gmail.com>
2021-01-14 21:20:55 -0800

    Add x-axis

--- scatterplot-graph/script.js
+++ scatterplot-graph/script.js
@@ -5,12 +5,33 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const drawScatterChart = (data) => {
+    console.log(data)
+
     const w = 1000;
     const h = 600;
+    const padding = 30;
 
     const svg = d3
         .select(".graph-container")
         .append("svg")
         .attr("width", w)
         .attr("height", h)
+
+
+    const xScale = d3
+        .scaleTime()
+        .domain([
+            new Date(`January 1, ${d3.min((data.map((d) => d["Year"])))} 00:00:00`),
+            new Date(`January 1, ${d3.max((data.map((d) => d["Year"])))} 00:00:00`)
+        ])
+        .range([padding, w - padding])
+    
+    const xAxis = d3.axisBottom(xScale);
+
+    svg
+        .append("g")
+        .attr("transform", `translate(0, ${h - padding})`)
+        .call(xAxis)
+        
+
 }
