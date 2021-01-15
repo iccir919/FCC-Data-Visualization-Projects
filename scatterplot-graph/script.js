2c520b541d8868c80d57772117ffb2953eb6558b
Neil Ricci <iccir919@gmail.com>
2021-01-15 09:42:22 -0800

    Add dots to graph

--- scatterplot-graph/script.js
+++ scatterplot-graph/script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json')
     .then(response => response.json())
-    .then(data => drawScatterChart(data))
+    .then(data => drawScatterGraph(data))
 });
 
-const drawScatterChart = (data) => {
-    console.log(data);
+const drawScatterGraph = (dataset) => {
+    console.log(dataset);
 
     const w = 1000;
     const h = 600;
@@ -20,21 +20,17 @@ const drawScatterChart = (data) => {
     const xScale = d3
         .scaleTime()
         .domain([
-            new Date(`January 1, ${d3.min((data.map((d) => d["Year"])))} 00:00:00`),
-            new Date(`January 1, ${d3.max((data.map((d) => d["Year"])))} 00:00:00`)
+            new Date(`January 1, ${d3.min((dataset.map((d) => d["Year"]))) - 1} 00:00:00`),
+            new Date(`January 1, ${d3.max((dataset.map((d) => d["Year"])))} 00:00:00`)
         ])
         .range([padding, w - padding])
 
-    const durations = data.map( d => moment.duration(`00:${d["Time"]}`)._milliseconds );
-    console.log(durations)
     const yScale = d3
         .scaleLinear()
-        .domain([d3.min(durations), d3.max(durations)])
+        .domain([d3.max(dataset.map(d => d["Seconds"])), d3.min(dataset.map(d => d["Seconds"]))])
         .range([h - padding, padding])
 
-    console.log(yScale(132900000))
-
-    const yAxis = d3.axisLeft(yScale).tickFormat(d => moment.unix(d / 1000).format("mm:ss") );
+    const yAxis = d3.axisLeft(yScale).tickFormat( (d) => formatDuration(d) );
     const xAxis = d3.axisBottom(xScale);
 
     svg
@@ -47,6 +43,32 @@ const drawScatterChart = (data) => {
         .append("g")
         .attr("transform", `translate(${padding}, 0)`)
         .call(yAxis)
-        
-    
+        .attr("id", "y-axis")
+
+    console.log(xScale(1995))
+
+    svg.selectAll("circle")
+        .data(dataset)
+        .enter()
+        .append("circle")
+        .attr("cy", d => yScale(d["Seconds"]))
+        .attr("cx", d => xScale( new Date(`January 1, ${d["Year"]} 00:00:00`) ))
+        .attr("r", 5)
+        .attr("class", "dot")
+        .attr("data-xvalue", d => new Date(`January 1, ${d["Year"]} 00:00:00`))
+        .attr("data-yvalue", d => d["Seconds"])
+        .attr("fill", "black")
 }
+
+const formatDuration = (duration) => {
+    const minutes = Math.floor(duration / 60);
+    const seconds = duration % 60;
+
+    return `${zeroPad(minutes)}:${zeroPad(seconds)}`
+}
+
+const zeroPad = (value) => {
+    if (value < 10) return `0${value}`;
+    else return value;
+}
+
