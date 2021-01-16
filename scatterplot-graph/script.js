5b67fd733c4255752b33ee6febdcd4780a27f24f
Neil Ricci <iccir919@gmail.com>
2021-01-16 09:40:21 -0800

    Add legend

--- scatterplot-graph/script.js
+++ scatterplot-graph/script.js
@@ -28,7 +28,7 @@ const drawScatterGraph = (dataset) => {
     const yScale = d3
         .scaleLinear()
         .domain([d3.max(dataset.map(d => d["Seconds"])), d3.min(dataset.map(d => d["Seconds"]))])
-        .range([h - padding, padding])
+        .range([h - padding, padding / 2])
 
     const yAxis = d3.axisLeft(yScale).tickFormat( (d) => formatDuration(d) );
     const xAxis = d3.axisBottom(xScale);
@@ -45,19 +45,66 @@ const drawScatterGraph = (dataset) => {
         .call(yAxis)
         .attr("id", "y-axis")
 
-    console.log(xScale(1995))
-
     svg.selectAll("circle")
         .data(dataset)
         .enter()
         .append("circle")
         .attr("cy", d => yScale(d["Seconds"]))
         .attr("cx", d => xScale( new Date(`January 1, ${d["Year"]} 00:00:00`) ))
-        .attr("r", 5)
+        .attr("r", 6)
         .attr("class", "dot")
         .attr("data-xvalue", d => new Date(`January 1, ${d["Year"]} 00:00:00`))
-        .attr("data-yvalue", d => d["Seconds"])
-        .attr("fill", "black")
+        .attr("data-yvalue", d => {
+            const minutes = d["Time"].split(":")[0];
+            const seconds = d["Time"].split(":")[1];
+
+            return new Date(`2020-02-02 00:${minutes}:${seconds}`)
+        })
+        .attr("fill", (d) => {
+            if(d["Doping"].length > 0) return "SteelBlue"
+            else return "Orange"
+        })
+        .attr("data-legend", (d) => {
+            if (d["Doping"].length > 0) return "Riders with doping allegations";
+            else return "No doping allegations";
+        })
+        .style("stroke-width", 1)
+        .style("stroke", "Black")
+
+    const legend = svg
+        .append("g")
+        .attr("id", "legend")    
+
+    legend
+        .append("rect")
+        .attr("x", w - padding)
+        .attr("y", h / 2 - 30)
+        .attr("height", 15)
+        .attr("width", 15)
+        .style("fill", "SteelBlue")
+    legend
+        .append("rect")
+        .attr("x", w - padding)
+        .attr("y", h / 2 - 60)
+        .attr("height", 15)
+        .attr("width", 15)
+        .style("fill", "Orange")
+    legend
+        .append("text")
+        .attr("x", w - padding - 130)
+        .attr("y", h / 2 - 50)
+        .text("No doping allegations")
+        .style("font-size", "13px")
+        .attr("alignment-baseline","bottom")
+        .attr("text-align", "left")
+
+    legend
+        .append("text")
+        .attr("x", w - padding - 180)
+        .attr("y", h / 2 - 23)
+        .text("Riders with doping allegations")
+        .style("font-size", "13px")
+        .attr("alignment-baseline","middle")
 }
 
 const formatDuration = (duration) => {
